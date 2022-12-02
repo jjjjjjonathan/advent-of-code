e4eab6087c7a18044961b787891575cd20968560
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-12-02 11:28:30 -0500

    Submit day2 part 1 answer

--- day02/answer.js
+++ day02/answer.js
@@ -3,4 +3,29 @@ const path = require('path');
 
 const data = fs.readFileSync(path.join(__dirname, 'input.txt'), { encoding: 'utf-8' });
 
-console.log(data);
+const rounds = data.split('\n').map((round) => round.split(' '));
+
+let points = 0;
+
+rounds.forEach((round) => {
+  if (round[1] === 'X') points += 1;
+  if (round[1] === 'Y') points += 2;
+  if (round[1] === 'Z') points += 3;
+
+  // wins
+  if (
+    (round[0] === 'A' && round[1] === 'Y') ||
+    (round[0] === 'B' && round[1] === 'Z') ||
+    (round[0] === 'C' && round[1] === 'X')
+  ) points += 6;
+
+  // draws
+
+  if (
+    (round[0] === 'A' && round[1] === 'X') ||
+    (round[0] === 'B' && round[1] === 'Y') ||
+    (round[0] === 'C' && round[1] === 'Z')
+  ) points += 3;
+});
+
+console.log(points);
