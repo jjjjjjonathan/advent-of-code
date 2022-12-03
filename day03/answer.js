368a177a9cbc46031925b896481d8e1cbddbdf52
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-12-03 15:33:06 -0500

    Answer day 3 part 2

--- day03/answer.js
+++ day03/answer.js
@@ -5,12 +5,12 @@ const inputData = fs.readFileSync(path.join(__dirname, 'input.txt'), {
   encoding: 'utf-8'
 });
 
-const organizedRucksacks = inputData
-  .split('\n')
-  .map((rucksack) => [
-    rucksack.slice(0, Math.floor(rucksack.length / 2)),
-    rucksack.slice(Math.floor(rucksack.length / 2))
-  ]);
+const rucksacks = inputData.split('\n');
+
+const organizedRucksacks = rucksacks.map((rucksack) => [
+  rucksack.slice(0, Math.floor(rucksack.length / 2)),
+  rucksack.slice(Math.floor(rucksack.length / 2))
+]);
 
 const priorityValues = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
@@ -30,3 +30,30 @@ organizedRucksacks.forEach((rucksack) => {
 });
 
 console.log('a: ', totalValue);
+
+// part 2
+
+const secondOrganizedSacks = [];
+
+for (let i = 0; i < rucksacks.length; i += 3) {
+  secondOrganizedSacks.push([rucksacks[i], rucksacks[i + 1], rucksacks[i + 2]]);
+}
+
+let secondTotalValue = 0;
+
+secondOrganizedSacks.forEach((rucksack) => {
+  let value = 0;
+
+  for (let i = 0; i < rucksack[0].length; i++) {
+    if (
+      rucksack[1].includes(rucksack[0][i]) &&
+      rucksack[2].includes(rucksack[0][i])
+    ) {
+      value = priorityValues.indexOf(rucksack[0][i]) + 1;
+    }
+    if (value > 0) break;
+  }
+  secondTotalValue += value;
+});
+
+console.log('b: ', secondTotalValue);
