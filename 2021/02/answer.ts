9832e9268c9f7b45aad0257245efe3b063e5db5f
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-12-04 13:21:21 -0500

    Answer 2021 day 2

---
+++ 2021/02/answer.ts
@@ -0,0 +1,41 @@
+import { parseInput } from '../../helpers';
+
+const data = parseInput(__dirname);
+
+const splitData = data.split('\n');
+
+const splitDirectionAndValue = splitData.map((command) => command.split(' '));
+
+let depth = 0;
+let horizontalPosition = 0;
+
+for (const command of splitDirectionAndValue) {
+  if (command[0] === 'up') {
+    depth -= parseInt(command[1]);
+  } else if (command[0] === 'down') {
+    depth += parseInt(command[1]);
+  } else {
+    horizontalPosition += parseInt(command[1]);
+  }
+}
+
+console.log('a: ', depth * horizontalPosition);
+
+// part two
+
+let depth2 = 0;
+let aim = 0;
+let horizontalPosition2 = 0;
+
+for (const command of splitDirectionAndValue) {
+  if (command[0] === 'up') {
+    aim -= parseInt(command[1]);
+  } else if (command[0] === 'down') {
+    aim += parseInt(command[1]);
+  } else {
+    horizontalPosition2 += parseInt(command[1]);
+    depth2 += aim * parseInt(command[1]);
+  }
+}
+
+console.log('b: ', depth2 * horizontalPosition2);
