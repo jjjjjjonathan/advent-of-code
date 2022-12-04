e56a871a5439f076effa110f7e4708cf7dc07950
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-12-04 13:03:07 -0500

    Answer 2021 day 1

--- 2021/01/answer.ts
+++ 2021/01/answer.ts
@@ -1,18 +1,30 @@
-import fs from 'fs';
-import path from 'path';
+import { parseInput } from '../../helpers';
 
-const data = fs.readFileSync(path.join(__dirname, 'input.txt'), {
-  encoding: 'utf-8',
-});
+const data = parseInput(__dirname);
 
 let count = 0;
 
-const splitData = data.split('\n');
+const splitData = data
+  .split('\n')
+  .map((measurement) => parseInt(measurement, 10));
 
 for (let i = 1; i < splitData.length; i++) {
-  if (parseInt(splitData[i]) > parseInt(splitData[i - 1])) {
+  if (splitData[i] > splitData[i - 1]) {
     count++;
   }
 }
 
 console.log('a: ', count);
+
+// part two
+
+let count2 = 0;
+
+for (let i = 0; i < splitData.length - 3; i++) {
+  const previousValue = splitData[i] + splitData[i + 1] + splitData[i + 2];
+  const nextValue = splitData[i + 1] + splitData[i + 2] + splitData[i + 3];
+
+  if (nextValue > previousValue) count2++;
+}
+
+console.log('b: ', count2);
