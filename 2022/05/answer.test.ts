e3e4416a837bb8ea75dc3fbf2a58b108d04f3ee4
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-12-05 10:14:33 -0500

    Start 2022 day 5 part 1

--- 2022/05/answer.test.ts
+++ 2022/05/answer.test.ts
@@ -1,16 +1,28 @@
 import { describe, expect, it } from 'vitest';
-import { parseInput } from '../../helpers';
+import { parseInput, splitData } from '../../helpers';
+import { organizeDrawing } from './answer';
 
 const data = parseInput(__dirname, 'sample.txt');
+const [drawing, moves] = splitData(data, 2);
 
 describe('setup', () => {
   it('parses the input text file', () => {
     expect(data).toBeTypeOf('string');
+    expect(drawing.includes('[D]')).toBe(true);
+    expect(moves.includes('move 1 from 2 to 1')).toBe(true);
+  });
+
+  it('organizes drawing into rows', () => {
+    const coordinates = organizeDrawing(drawing);
+    expect(Array.isArray(coordinates[0])).toBe(true);
+    expect(coordinates[0][0]).toBe(' ');
+    expect(coordinates[0][1]).toBe('D');
+    expect(coordinates[0][2]).toBe(' ');
   });
 });
 
 describe('answer', () => {
-  const answer = 'CMX';
+  const answer = 'CMZ';
   it('should be a string', () => {
     expect(answer).toBeTypeOf('string');
     expect(answer).toBe('CMZ');
--- 2022/05/answer.ts
+++ 2022/05/answer.ts
@@ -1,5 +1,19 @@
-import { parseInput } from '../../helpers';
+import { parseInput, splitData } from '../../helpers';
 
 const data = parseInput(__dirname, 'input.txt');
+const [drawing, moves] = splitData(data, 2);
 
-console.log(data);
+export const organizeDrawing = (drawing: string) => {
+  const rows = splitData(drawing, 1);
+  rows.pop();
+
+  return rows.map((row) => {
+    let temp: string[] = [];
+    for (let i = 1; i < row.length; i += 4) {
+      temp.push(row[i]);
+    }
+    return temp;
+  });
+};
+
+console.log(organizeDrawing(drawing));
