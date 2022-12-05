a7c04c249c20cc6092c4f8e57fcf16250e34eb5a
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-12-05 11:59:52 -0500

    Answer 2022 day 5 part 1

--- 2022/05/answer.test.ts
+++ 2022/05/answer.test.ts
@@ -1,9 +1,11 @@
 import { describe, expect, it } from 'vitest';
 import { parseInput, splitData } from '../../helpers';
-import { organizeDrawing } from './answer';
+import { organizeDrawing, organizeMoves, getAnswer, makeMoves } from './answer';
 
 const data = parseInput(__dirname, 'sample.txt');
 const [drawing, moves] = splitData(data, 2);
+const coordinates = organizeDrawing(drawing);
+const steps = organizeMoves(moves);
 
 describe('setup', () => {
   it('parses the input text file', () => {
@@ -15,14 +17,23 @@ describe('setup', () => {
   it('organizes drawing into rows', () => {
     const coordinates = organizeDrawing(drawing);
     expect(Array.isArray(coordinates[0])).toBe(true);
-    expect(coordinates[0][0]).toBe(' ');
-    expect(coordinates[0][1]).toBe('D');
-    expect(coordinates[0][2]).toBe(' ');
+    expect(coordinates[0][0]).toBe('Z');
+    expect(coordinates[1][0]).toBe('M');
+    expect(coordinates[2][0]).toBe('P');
+    expect(coordinates[0].includes(' ')).toBe(false);
+  });
+
+  it('leaves only numbers for moves', () => {
+    const steps = organizeMoves(moves);
+    expect(Array.isArray(steps[0])).toBe(true);
+    expect(steps[0][0]).toBe(1);
+    expect(steps[0][1]).toBe(1);
+    expect(steps[0][2]).toBe(0);
   });
 });
 
 describe('answer', () => {
-  const answer = 'CMZ';
+  const answer = getAnswer(makeMoves(coordinates, steps) as string[][]);
   it('should be a string', () => {
     expect(answer).toBeTypeOf('string');
     expect(answer).toBe('CMZ');
--- 2022/05/answer.ts
+++ 2022/05/answer.ts
@@ -3,17 +3,94 @@ import { parseInput, splitData } from '../../helpers';
 const data = parseInput(__dirname, 'input.txt');
 const [drawing, moves] = splitData(data, 2);
 
-export const organizeDrawing = (drawing: string) => {
+export const organizeDrawing = (drawing: string): string[][] => {
   const rows = splitData(drawing, 1);
   rows.pop();
 
-  return rows.map((row) => {
+  const coordinates = rows.map((row) => {
     let temp: string[] = [];
     for (let i = 1; i < row.length; i += 4) {
       temp.push(row[i]);
     }
     return temp;
   });
+
+  const transposed: string[][] = new Array(coordinates.length);
+
+  for (let i = coordinates.length - 1; i >= 0; i--) {
+    for (let j = 0; j < coordinates[i].length; j++) {
+      if (i === coordinates.length - 1) {
+        transposed[j] = [coordinates[i][j]];
+      } else if (coordinates[i][j] !== ' ') {
+        transposed[j].push(coordinates[i][j]);
+      }
+    }
+  }
+
+  return transposed;
 };
 
-console.log(organizeDrawing(drawing));
+export const organizeMoves = (moves: string): number[][] => {
+  const rows = splitData(moves, 1);
+
+  const steps = rows.map((row) => {
+    const wordsRemoved = row.replace(/[A-Za-z]/gi, '');
+    return wordsRemoved
+      .split(' ')
+      .filter((element) => element.length > 0)
+      .map((number, index) =>
+        index === 0 ? parseInt(number) : parseInt(number) - 1
+      );
+  });
+  return steps;
+};
+
+const transposedDrawing = organizeDrawing(drawing);
+const moveQueue = organizeMoves(moves);
+
+const makeStep = (
+  drawing: string[][],
+  count: number,
+  prevLocation: number,
+  newLocation: number
+): string[][] | undefined => {
+  const crate: string | undefined = drawing[prevLocation].pop();
+
+  drawing[newLocation].push(crate as string);
+
+  count--;
+
+  if (count > 0) {
+    return makeStep(drawing, count, prevLocation, newLocation);
+  } else {
+    return drawing;
+  }
+};
+
+export const makeMoves = (
+  drawing: string[][],
+  queue: number[][]
+): string[][] | undefined => {
+  const [count, prevLocation, newLocation] = queue[0];
+
+  const completeStep = makeStep(drawing, count, prevLocation, newLocation);
+
+  queue.shift();
+
+  if (queue.length > 0) {
+    return makeMoves(completeStep as string[][], queue);
+  } else {
+    return completeStep;
+  }
+};
+
+export const getAnswer = (drawing: string[][]): string => {
+  let stringAnswer = '';
+  for (let i = 0; i < drawing.length; i++) {
+    stringAnswer += drawing[i][drawing[i].length - 1];
+  }
+
+  return stringAnswer;
+};
+
+console.log(getAnswer(makeMoves(transposedDrawing, moveQueue) as string[][]));
