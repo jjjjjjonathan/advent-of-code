8ad4beabc42d78ff6dc6abbc5418eaf917a956d7
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-12-10 11:47:24 -0500

    Find tail path for when head moves and stays in same row or column

--- 2022/09/2022-09.test.ts
+++ 2022/09/2022-09.test.ts
@@ -1,9 +1,12 @@
 import { parseInput, splitData } from '../../helpers';
 import { describe, it, expect } from 'vitest';
-import { getAnswer1} from './2022-09'
+import { getAnswer1, makeMove } from './2022-09';
 
 const data = parseInput(__dirname, 'sample.txt');
 const motions = splitData(data, 1).map((motion) => motion.split(' '));
+const startingPosition = [0, 0];
+const whereTailHasBeen = [startingPosition];
+
 describe('setup', () => {
   it('gives a string and a string-number for direction and number of steps', () => {
     expect(motions[0][0]).toBeTypeOf('string');
@@ -11,11 +14,44 @@ describe('setup', () => {
     expect(motions[0][1]).toBeTypeOf('string');
     expect(motions[0][1]).toBe('4');
   });
+
+  it('makes a move to right, and stays in same row as tail', () => {
+    const firstMove = makeMove(motions[0], [...whereTailHasBeen]);
+    expect(firstMove.currentPosition[0]).toBe(0);
+    expect(firstMove.currentPosition[1]).toBe(4);
+    expect(firstMove.whereTailHasBeen[0][0]).toBe(0);
+    expect(firstMove.whereTailHasBeen[0][1]).toBe(3);
+  });
+
+  it('makes a move to left, and stays in same row as tail', () => {
+    const thirdMove = makeMove(motions[2], [...whereTailHasBeen]);
+    expect(thirdMove.currentPosition[0]).toBe(0);
+    expect(thirdMove.currentPosition[1]).toBe(-3);
+    expect(thirdMove.whereTailHasBeen[0][0]).toBe(0);
+    expect(thirdMove.whereTailHasBeen[0][1]).toBe(-2);
+  });
+
+  it('makes a move up, and stays in same column as tail', () => {
+    const secondMove = makeMove(motions[1], [...whereTailHasBeen]);
+    expect(secondMove.currentPosition[0]).toBe(4);
+    expect(secondMove.currentPosition[1]).toBe(0);
+    expect(secondMove.whereTailHasBeen[0][0]).toBe(3);
+    expect(secondMove.whereTailHasBeen[0][1]).toBe(0);
+  });
+
+  it('makes a move down, and stays in same column as tail', () => {
+    const fourthMove = makeMove(motions[3], [...whereTailHasBeen]);
+    expect(fourthMove.currentPosition[0]).toBe(-1);
+    expect(fourthMove.currentPosition[1]).toBe(0);
+    expect(fourthMove.whereTailHasBeen[0][0]).toBe(0);
+    expect(fourthMove.whereTailHasBeen[0][1]).toBe(0);
+    console.log(fourthMove.whereTailHasBeen);
+  });
 });
 
 describe('first part', () => {
-  it('gives the correct answer', () =>{
+  it('gives the correct answer', () => {
     const answer = getAnswer1();
-    expect(answer).toBe(13)
-  })
-})
+    expect(answer).toBe(13);
+  });
+});
--- 2022/09/2022-09.ts
+++ 2022/09/2022-09.ts
@@ -4,10 +4,64 @@ const data = parseInput(__dirname, 'sample.txt');
 const motions = splitData(data, 1).map((motion) => motion.split(' '));
 
 const startingPosition = [0, 0];
-const whereTailHasBeen = [startingPosition]
+const whereTailHasBeen = [startingPosition];
+
+type MakeMove = {
+  currentPosition: number[];
+  whereTailHasBeen: number[][];
+};
+
+export const makeMove = (
+  move: string[],
+  whereTailHasBeen: number[][]
+): MakeMove => {
+  const previousPosition = [...whereTailHasBeen[0]];
+  const currentPosition = [...whereTailHasBeen[0]];
+
+  if (move[0] === 'R') {
+    currentPosition[1] += parseInt(move[1]);
+
+    if (previousPosition[0] === currentPosition[0]) {
+      for (let i = previousPosition[1] + 1; i < currentPosition[1]; i++) {
+        whereTailHasBeen.unshift([previousPosition[0], i]);
+      }
+    }
+  }
+
+  if (move[0] === 'L') {
+    currentPosition[1] -= parseInt(move[1]);
+
+    if (previousPosition[0] === currentPosition[0]) {
+      for (let i = previousPosition[1] - 1; i > currentPosition[1]; i--) {
+        whereTailHasBeen.unshift([previousPosition[0], i]);
+      }
+    }
+  }
+
+  if (move[0] === 'U') {
+    currentPosition[0] += parseInt(move[1]);
+
+    if (previousPosition[1] === currentPosition[1]) {
+      for (let i = previousPosition[0] + 1; i < currentPosition[0]; i++) {
+        whereTailHasBeen.unshift([i, previousPosition[1]]);
+      }
+    }
+  }
+
+  if (move[0] === 'D') {
+    currentPosition[0] -= parseInt(move[1]);
+
+    if (previousPosition[1] === currentPosition[1]) {
+      for (let i = previousPosition[0] - 1; i > currentPosition[0]; i--) {
+        whereTailHasBeen.unshift([i, previousPosition[1]]);
+      }
+    }
+  }
+  return { currentPosition, whereTailHasBeen };
+};
 
 export const getAnswer1 = () => {
   return 13;
-}
+};
 
-console.log(getAnswer1())
+console.log(getAnswer1());
