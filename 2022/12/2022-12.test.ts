8f905a261b97415ca8358d063dc59a1e60461d29
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-12-12 12:03:36 -0500

    Find start and end point for 2022-12

---
+++ 2022/12/2022-12.test.ts
@@ -0,0 +1,20 @@
+import { parseInput, splitData } from '../../helpers';
+import { describe, it, expect } from 'vitest';
+import { findPoint, START, END } from './2022-12';
+
+const data = parseInput(__dirname, 'sample.txt');
+const rows = splitData(data, 1).map((row) => row.split(''));
+
+describe('setup', () => {
+  it('finds the start', () => {
+    const start = findPoint(rows, START);
+    expect(start[0]).toBe(0);
+    expect(start[1]).toBe(0);
+  });
+
+  it('finds the end', () => {
+    const end = findPoint(rows, END);
+    expect(end[0]).toBe(2);
+    expect(end[1]).toBe(5);
+  });
+});
---
+++ 2022/12/2022-12.ts
@@ -0,0 +1,22 @@
+import { parseInput, splitData } from '../../helpers';
+
+const data = parseInput(__dirname, 'sample.txt');
+const rows = splitData(data, 1).map((row) => row.split(''));
+export const START = 'S';
+export const END = 'E';
+
+export const findPoint = (rows: string[][], point: string): number[] => {
+  let pointToReturn: number[] = [-1, -1];
+  for (let y = 0; y < rows.length; y++) {
+    for (let x = 0; x < rows[y].length; x++) {
+      if (rows[y][x] === point) {
+        pointToReturn = [y, x];
+        break;
+      }
+    }
+    if (pointToReturn[0] > -1 && pointToReturn[1] > -1) {
+      break;
+    }
+  }
+  return pointToReturn;
+};
