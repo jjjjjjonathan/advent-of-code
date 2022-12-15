e2d7fa0735b015cc18d301da53791643bd5824b4
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-12-14 21:46:00 -0500

    Compare number values for 2022-13

--- 2022/13/2022-13.ts
+++ 2022/13/2022-13.ts
@@ -1,6 +1,10 @@
 import { parseInput, splitData } from '../../helpers';
 
 const data = parseInput(__dirname, 'sample.txt');
-const pairs = splitData(data, 2).map((pair) => pair.split('\n'));
+const pairs: (number | object)[][] = splitData(data, 2).map((pair) => pair.split('\n').map((element) => JSON.parse(element)));
 
-console.log(pairs);
+export const compareNumbers = (leftValue: number, rightValue: number): number => {
+  if (leftValue < rightValue) return 1;
+  if (leftValue > rightValue) return -1;
+  return 0;
+}
